56817c3de44e2bef4089267345c5c2e00c061fe7
david guzikowski <guzikowski.david@gmail.com>
2021-04-25 16:02:44 -0400

    Week 3 Assignment NodeServer

--- app.js
+++ app.js
@@ -1,85 +1,68 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-const passport = require('passport');
-const config = require('./config');
+const passport= require("passport")
+const config = require("./config")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require("./routes/campsiteRouter")
+const promotionRouter = require("./routes/promotionRouter")
+const partnersRouter = require("./routes/partnersRouter")
+
+const mongoose= require("mongoose")
+
+const url = config.mongoUrl
+const connect = mongoose.connect(url,{
+  useCreateIndex: true, 
+  useFindAndModify: false, 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+connect.then(()=>console.log("Connected correctly to server"), err => console.log(err))
 
 var app = express();
 
-const campsiteRouter = require('./routes/campsiteRouter');
-const promotionRouter = require('./routes/promotionRouter');
-const partnerRouter = require('./routes/partnerRouter');
+app.all("*",(req,res,next)=>{
+  if(req.secure)
+  {
+    return next()
+  }
+  else
+  {
+    console.log(`Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`);
+    res.redirect(301,`https://${req.hostname}:${app.get("secPort")}${req.url}`)
+  }
+})
 
-const mongoose = require('mongoose');
-
-const url = config.mongoUrl;
-const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-});
-
-connect.then(() => console.log('Connected correctly to server'), 
-    err => console.log(err)
-);
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(passport.initialize());
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.session);
+app.use(express.static(path.join(__dirname, 'public')));
 
-  if (!req.session.user) {
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  } else {
-    if (req.session.user === 'authenticated') {
-      return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
-  }
-}
+app.use("/campsites", campsiteRouter)
+app.use("/promotions", promotionRouter)
+app.use("/partners", partnersRouter)
 
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
